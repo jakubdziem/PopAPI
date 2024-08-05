e69803d0b765307d0ff11b70b409c90a9f7f4613
Jakub Dziem <jakub.dziem9@gmail.com>
2024-08-05 16:22:55 +0200

    Handling unsuitable input - year.

--- src/main/java/com/dziem/popapi/model/CountryDTO.java
+++ src/main/java/com/dziem/popapi/model/CountryDTO.java
@@ -1,10 +1,12 @@
 package com.dziem.popapi.model;
 
+import lombok.Builder;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
+@Builder
 public class CountryDTO {
     private String countryName;
     private List<YearAndPopulation> yearAndPopulations;
--- src/main/java/com/dziem/popapi/service/CountryServiceImpl.java
+++ src/main/java/com/dziem/popapi/service/CountryServiceImpl.java
@@ -22,7 +22,14 @@ public class CountryServiceImpl implements CountryService{
     public List<CountryDTO> findCountriesByDistinctYear(String year) {
         List<Country> allCountries = countryRepository.findAll();
         List<YearAndPopulation> allYearsAndPopulations = yearAndPopulationRepository.findAll();
-        int yearNum = Integer.parseInt(year);
+        int yearNum;
+        try {
+            yearNum = Integer.parseInt(year);
+        } catch (NumberFormatException e) {
+            ArrayList<CountryDTO> emptyList = new ArrayList<>();
+            emptyList.add(CountryDTO.builder().countryName("This is not a year, try something in this format: /api/v1/pop/2024").build());
+            return emptyList;
+        }
         List<Country> countries;
         List<YearAndPopulation> yearAndPopulationList;
         switch(yearNum) {
@@ -46,28 +53,40 @@ public class CountryServiceImpl implements CountryService{
                 break;
             }
             default: {
-                countries = allCountries.stream().filter(c ->
-                        (!c.getCountryName().endsWith("1989") &&
-                        !c.getCountryName().endsWith("1900") &&
-                        !c.getCountryName().endsWith("1939"))).toList();
-                yearAndPopulationList = allYearsAndPopulations.stream().filter(yearAndPopulation ->
-                        (!yearAndPopulation.getCountry().getCountryName().endsWith("1989") &&
-                                !yearAndPopulation.getCountry().getCountryName().endsWith("1900") &&
-                                !yearAndPopulation.getCountry().getCountryName().endsWith("1939"))
-                                && yearAndPopulation.getYearOfMeasurment().equals(year)).toList();
+                if(yearNum >= 2024 && yearNum <= 2150) {
+                    countries = allCountries.stream().filter(c ->
+                            (!c.getCountryName().endsWith("1989") &&
+                                    !c.getCountryName().endsWith("1900") &&
+                                    !c.getCountryName().endsWith("1939"))).toList();
+                    yearAndPopulationList = allYearsAndPopulations.stream().filter(yearAndPopulation ->
+                            (!yearAndPopulation.getCountry().getCountryName().endsWith("1989") &&
+                                    !yearAndPopulation.getCountry().getCountryName().endsWith("1900") &&
+                                    !yearAndPopulation.getCountry().getCountryName().endsWith("1939"))
+                                    && yearAndPopulation.getYearOfMeasurment().equals(year)).toList();
+                } else {
+                    ArrayList<CountryDTO> emptyList = new ArrayList<>();
+                    emptyList.add(CountryDTO.builder().countryName("The " + year + " is not in the database.")
+                            .flagUrl(" Try one of these: /api/v1/pop/1900, /api/v1/pop/1939, /api/v1/pop/1989, or from the 2024-2150 range").build());
+                    return emptyList;
+                }
                 break;
             }
         }
-
         List<CountryDTO> countryDTOS = new ArrayList<>();
-        for(int i = 0; i < countries.size(); i++) {
-            List<YearAndPopulation> singleYearAndPopulation = new ArrayList<>();
-            singleYearAndPopulation.add(yearAndPopulationList.get(i));
+        if(!countries.isEmpty()) {
+            for (int i = 0; i < countries.size(); i++) {
+                List<YearAndPopulation> singleYearAndPopulation = new ArrayList<>();
+                singleYearAndPopulation.add(yearAndPopulationList.get(i));
 
-            countries.get(i).setYearAndPopulations(singleYearAndPopulation);
+                countries.get(i).setYearAndPopulations(singleYearAndPopulation);
 
-            CountryDTO countryDTO = countryMapper.countryToCountryDto(countries.get(i));
-            countryDTOS.add(countryDTO);
+                CountryDTO countryDTO = countryMapper.countryToCountryDto(countries.get(i));
+                countryDTOS.add(countryDTO);
+            }
+        } else {
+            ArrayList<CountryDTO> emptyList = new ArrayList<>();
+            emptyList.add(CountryDTO.builder().countryName("List for this year:" + year + "is empty.").build());
+            return emptyList;
         }
         return countryDTOS;
     }
