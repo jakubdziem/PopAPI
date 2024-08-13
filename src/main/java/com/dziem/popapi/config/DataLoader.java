4fdf5c002e4a387898fc6c4241048c82e248b83f
Jakub Dziem <jakub.dziem9@gmail.com>
2024-08-13 13:17:40 +0200

    Images and data update for spotify and flag and data update for 1939 population

--- src/main/java/com/dziem/popapi/config/DataLoader.java
+++ src/main/java/com/dziem/popapi/config/DataLoader.java
@@ -1,5 +1,6 @@
 package com.dziem.popapi.config;
 
+import com.dziem.popapi.formatter.ArtistNameListGetter;
 import com.dziem.popapi.service.DataService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
@@ -10,10 +11,12 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class DataLoader implements CommandLineRunner {
     private final DataService dataService;
+    private final ArtistNameListGetter artistNameListGetter;
     @Override
     public void run(String... args) throws Exception {
         dataService.getData2024_2100();
         dataService.getData2100_2150();
         dataService.getDataSpotifyTopArtists();
+        artistNameListGetter.getList();
     }
 }
---
+++ src/main/java/com/dziem/popapi/formatter/ArtistNameListGetter.java
@@ -0,0 +1,36 @@
+package com.dziem.popapi.formatter;
+
+import com.dziem.popapi.model.Artist;
+import com.dziem.popapi.repository.ArtistRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+@Component
+@RequiredArgsConstructor
+public class ArtistNameListGetter {
+    private final ArtistRepository artistRepository;
+    public void getList() {
+        List<Artist> artists = artistRepository.findAll();
+        List<String> artistsNames = new ArrayList<>();
+        for(Artist artist : artists) {
+            artistsNames.add(artist.getArtistName());
+        }
+        try {
+            FileWriter myWriter = new FileWriter("src/main/resources/spotifyArtistsInFormatSuitableToExtractImages.txt");
+            for(int i = 0; i < artistsNames.size(); i++) {
+                myWriter.write(i==artistsNames.size()-1 ? "\""+artistsNames.get(i)+"\"" :"\""+artistsNames.get(i)+"\",\n");
+            }
+            myWriter.close();
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+//        src/main/resources/spotifyArtistsInFormatSuitableToExtractImages.txt
+
+    }
+
+}
