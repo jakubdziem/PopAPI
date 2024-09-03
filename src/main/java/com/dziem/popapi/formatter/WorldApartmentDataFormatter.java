9c5b032545e769f396824dd25cf5702ac73010db
Jakub Dziem <jakub.dziem9@gmail.com>
2024-09-03 20:41:48 +0200

    Data for apartmentPricesPoland.

--- src/main/java/com/dziem/popapi/formatter/WorldApartmentDataFormatter.java
+++ src/main/java/com/dziem/popapi/formatter/WorldApartmentDataFormatter.java
@@ -7,6 +7,31 @@ import java.util.stream.Stream;
 
 public class WorldApartmentDataFormatter {
     public static void main(String[] args) {
+//        worldApartment();
+        polandApartment();
+    }
+
+    private static void polandApartment() {
+        try (Stream<String> stream = Files.lines(Paths.get("src/main/resources/data/apartmentPricesPoland.txt"))) {
+            List<String> lines = stream.toList();
+            String name = "";
+            for(int i = 0; i < lines.size(); i++) {
+                if(i%2==0) {
+                    name = lines.get(i);
+                }
+                else {
+                    Float price = Float.parseFloat(lines.get(i));
+                    String format = String.format("%.2f", price);
+                    System.out.printf("INSERT INTO APARTMENT (NAME, PRICE, CATEGORY, IMAGE_URL) VALUES ('%s', %s, 'Poland','/images/city/poland/%s');\n", name, format.replace(",", "."), name);
+                }
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void worldApartment() {
         try (Stream<String> stream = Files.lines(Paths.get("src/main/resources/data/apartmentPricesWorld.txt"))) {
             List<String> lines = stream.toList();
             for(String line : lines) {
