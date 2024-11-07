a1d2ca9958fcb12e415e95c0069eada3b6089f68
Jakub Dziem <jakub.dziem9@gmail.com>
2024-11-07 21:11:05 +0100

    Changed mapping of stats page and now there is a selection of which mode a user is willing to see the stats of.

--- src/main/java/com/dziem/popapi/controller/StatsPageController.java
+++ src/main/java/com/dziem/popapi/controller/StatsPageController.java
@@ -5,10 +5,13 @@ import com.dziem.popapi.model.webpage.StatsWithUName;
 import com.dziem.popapi.service.StatsPageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
@@ -16,14 +19,6 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class StatsPageController {
     private final StatsPageService statsPageService;
-    @GetMapping("/stats")
-    public String getStatsPage() {
-        return "stats";
-    }
-    @ModelAttribute("overallStats")
-    public List<StatsWithUName> getStatsWithUNameOfAllUsers() {
-        return statsPageService.getStatsWithUNameOfAllUsers();
-    }
     @ModelAttribute("overallStatsOfUsersCombined")
     public StatsWithUName getStatsOfAllUsersCombined() {
         return statsPageService.getStatsOfAllUsersCombined();
@@ -33,14 +28,24 @@ public class StatsPageController {
     public List<StatsWithUName> getStatsWithUNameOfAllUsersForChart() {
         return statsPageService.getStatsWithUNameOfAllUsers();
     }
-    @ModelAttribute("overallStatsPerMode")
-    public Map<Mode, List<StatsWithUName>> getAllGameStatsWithUNameOfAllUsers()
-    {
-        return statsPageService.getAllGameStatsWithUNameOfAllUsers();
-    }
     @ModelAttribute("overallStatsOfUsersPerMode")
     public Map<Mode, StatsWithUName> getGameStatsOffAllUsersCombined() {
         return statsPageService.getGameStatsOffAllUsersCombined();
     }
+
+    @GetMapping("/stats")
+    public String showStats(@RequestParam(defaultValue = "GENERAL") String selectedMode, Model model) {
+        model.addAttribute("selectedMode", selectedMode);
+        List<String> modes = Arrays.stream(Mode.values()).map(Enum::toString).toList();
+        model.addAttribute("modes", modes);
+
+        if ("GENERAL".equals(selectedMode)) {
+            model.addAttribute("overallStats", statsPageService.getStatsWithUNameOfAllUsers());
+        } else {
+            model.addAttribute("overallStatsPerMode", statsPageService.getAllGameStatsWithUNameOfAllUsers());
+        }
+        return "stats";
+    }
+
 
 }
