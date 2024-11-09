081dc5947c14b461a97797af7ee35883a77da278
Jakub Dziem <jakub.dziem9@gmail.com>
2024-11-09 16:50:26 +0100

    Modeled all attributes for week and week attribute.

--- src/main/java/com/dziem/popapi/controller/StatsPageController.java
+++ src/main/java/com/dziem/popapi/controller/StatsPageController.java
@@ -7,11 +7,9 @@ import com.dziem.popapi.service.StatsPageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
@@ -35,12 +33,12 @@ public class StatsPageController {
     }
 
     @GetMapping("/stats")
-    public String showStats(@RequestParam(defaultValue = "GENERAL") String selectedMode, Model model) {
+    public String showStats(@RequestParam(defaultValue = "COMBINED_STATS") String selectedMode, Model model) {
         model.addAttribute("selectedMode", selectedMode);
         List<String> modes = Arrays.stream(Mode.values()).map(Enum::toString).toList();
         model.addAttribute("modes", modes);
-
-        if ("GENERAL".equals(selectedMode)) {
+        model.addAttribute("weeks", statsPageService.getWeeks());
+        if ("COMBINED_STATS".equals(selectedMode)) {
             model.addAttribute("overallStats", statsPageService.getStatsWithUNameOfAllUsersCurrent());
         } else {
             model.addAttribute("overallStatsPerMode", statsPageService.getAllGameStatsWithUNameOfAllUsersCurrent());
@@ -51,5 +49,26 @@ public class StatsPageController {
     @ModelAttribute("users")
     public UsersSummed getUsersSummed() {
         return statsPageService.getUsersSummedCurrent();
+    }
+    @ModelAttribute("overallStatsOfUsersPerModeWeekly")
+    public Map<String, StatsWithUName> getSummedStatsPerModeFromWeek() {
+        return statsPageService.getGameStatsOffAllUsersCombinedFromWeek(LocalDate.parse("2024-11-09"));
+    }
+    @ModelAttribute("overallStatsOfUsersCombinedWeekly")
+    public StatsWithUName getSummedStatsFromWeek() {
+        System.out.println(statsPageService.getStatsOfAllUsersCombinedFromWeek(LocalDate.parse("2024-11-09")));
+        return statsPageService.getStatsOfAllUsersCombinedFromWeek(LocalDate.parse("2024-11-09"));
+    }
+    @ModelAttribute("usersWeekly")
+    public UsersSummed getUsersSummedFromWeek() {
+        return statsPageService.getUsersSummedFromWeek(LocalDate.parse("2024-11-09"));
+    }
+    @ModelAttribute("overallStatsPerModeWeekly")
+    public Map<String, List<StatsWithUName>> getAllGameStatsWithUNameOfAllUsersFromWeek() {
+        return statsPageService.getAllGameStatsWithUNameOfAllUsersFromWeek(LocalDate.parse("2024-11-09"));
+    }
+    @ModelAttribute("overallStatsWeekly")
+    public List<StatsWithUName> getStatsWithUNameOfAllUsersFromWeek() {
+        return statsPageService.getStatsWithUNameOfAllUsersFromWeek(LocalDate.parse("2024-11-09"));
     }
 }
