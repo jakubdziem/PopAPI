0aa1021278d5fc47490fae465fe693335f32c42e
Jakub Dziem <jakub.dziem9@gmail.com>
2024-11-11 19:40:54 +0100

    Added StatsPageChartService with three methods saveDailySummedStatsSnapshot with cron job at 6am +1GMT Zone, populateWeek and getDailyStatsSummedForChartPerMode. Implemented two of them saveDailySummedStatsSnapshot, populateWeek.

---
+++ src/main/java/com/dziem/popapi/service/StatsPageChartService.java
@@ -0,0 +1,12 @@
+package com.dziem.popapi.service;
+
+import com.dziem.popapi.model.webpage.DailyStatsSummed;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface StatsPageChartService {
+    void saveDailySummedStatsSnapshot();
+    void populateWeek(LocalDate week);
+    List<DailyStatsSummed> getDailyStatsSummedForChartPerMode(String mode);
+}
---
+++ src/main/java/com/dziem/popapi/service/StatsPageChartServiceImpl.java
@@ -0,0 +1,131 @@
+package com.dziem.popapi.service;
+
+import com.dziem.popapi.model.Mode;
+import com.dziem.popapi.model.webpage.DailyStatsSummed;
+import com.dziem.popapi.model.webpage.StatsWithUName;
+import com.dziem.popapi.model.webpage.TimeConverter;
+import com.dziem.popapi.repository.DailyStatsSummedRepository;
+import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import static com.dziem.popapi.service.StatsPageServiceImpl.COMBINED_STATS;
+
+@Service
+@AllArgsConstructor
+public class StatsPageChartServiceImpl implements StatsPageChartService {
+    private static final Logger logger = LoggerFactory.getLogger(StatsPageServiceImpl.class);
+    private final StatsPageService statsPageService;
+    private final DailyStatsSummedRepository dailyStatsSummedRepository;
+    @Scheduled(cron = "0 0 0 * * *")
+    @Override
+    public void saveDailySummedStatsSnapshot() {
+        logger.info("Starting daily summed stats snapshot...");
+        DayOfWeek dayOfWeek = LocalDate.now().getDayOfWeek();
+        String weekStr = LocalDate.now().toString();
+        StatsWithUName stats = statsPageService.getDifferenceStatsOfAllUsersCombined(weekStr);
+        Map<String, StatsWithUName> gameStats = statsPageService.getDifferenceGameStatsOffAllUsersCombined(weekStr);
+        List<DailyStatsSummed> dailyStats = new ArrayList<>();
+        LocalDate day = LocalDate.now();
+        if(!dayOfWeek.equals(DayOfWeek.SUNDAY)) {
+            List<DailyStatsSummed> dailySummedStatsPrevDay = dailyStatsSummedRepository.findAllByDay(day.minusDays(1));
+            DailyStatsSummed combinedStatsPrevDay = dailySummedStatsPrevDay.stream().filter(stat -> stat.getMode().equals(COMBINED_STATS)).toList().get(0);
+            dailyStats.add(DailyStatsSummed.builder()
+                    .day(day)
+                    .mode(COMBINED_STATS)
+                    .totalGamePlayed(stats.getTotalGamePlayed() - combinedStatsPrevDay.getTotalGamePlayed())
+                    .avgScore(stats.getAvgScore().subtract(combinedStatsPrevDay.getAvgScore()))
+                    .timePlayed(TimeConverter.differenceOfTime(stats.getTimePlayed(), combinedStatsPrevDay.getTimePlayed()))
+                    .totalScoredPoints(stats.getTotalScoredPoints() - combinedStatsPrevDay.getTotalScoredPoints())
+                    .numberOfWonGames(stats.getNumberOfWonGames() - combinedStatsPrevDay.getNumberOfWonGames())
+                    .build());
+            for(String mode : gameStats.keySet()) {
+                combinedStatsPrevDay = dailySummedStatsPrevDay.stream().filter(stat -> stat.getMode().equals(mode)).toList().get(0);
+                dailyStats.add(DailyStatsSummed.builder()
+                        .day(day)
+                        .mode(mode)
+                        .totalGamePlayed(gameStats.get(mode).getTotalGamePlayed() - combinedStatsPrevDay.getTotalGamePlayed())
+                        .avgScore(gameStats.get(mode).getAvgScore().subtract(combinedStatsPrevDay.getAvgScore()))
+                        .timePlayed(TimeConverter.differenceOfTime(gameStats.get(mode).getTimePlayed(), combinedStatsPrevDay.getTimePlayed()))
+                        .totalScoredPoints(gameStats.get(mode).getTotalScoredPoints() - combinedStatsPrevDay.getTotalScoredPoints())
+                        .numberOfWonGames(gameStats.get(mode).getNumberOfWonGames()  - combinedStatsPrevDay.getNumberOfWonGames())
+                        .build());
+            }
+        }
+        dailyStats.add(DailyStatsSummed.builder()
+                .day(day)
+                .mode(COMBINED_STATS)
+                .totalGamePlayed(stats.getTotalGamePlayed())
+                .avgScore(stats.getAvgScore())
+                .timePlayed(stats.getTimePlayed())
+                .totalScoredPoints(stats.getTotalScoredPoints())
+                .numberOfWonGames(stats.getNumberOfWonGames())
+                .build());
+        for(String mode : gameStats.keySet()) {
+            dailyStats.add(DailyStatsSummed.builder()
+                    .day(day)
+                    .mode(mode)
+                    .totalGamePlayed(gameStats.get(mode).getTotalGamePlayed())
+                    .avgScore(gameStats.get(mode).getAvgScore())
+                    .timePlayed(gameStats.get(mode).getTimePlayed())
+                    .totalScoredPoints(gameStats.get(mode).getTotalScoredPoints())
+                    .numberOfWonGames(gameStats.get(mode).getNumberOfWonGames())
+                    .build());
+        }
+        dailyStatsSummedRepository.saveAll(dailyStats);
+        logger.info("Daily summed stats snapshot completed.");
+    }
+
+    private DailyStatsSummed getDailyStatsSummedFromDayAndPerMode(StatsWithUName stats ,int i, String mode, LocalDate week) {
+        long restTotalGamePlayed = stats.getTotalGamePlayed() % 7;
+        long totalGamePlayedPerDay = stats.getTotalGamePlayed() / 7;
+        long seconds = TimeConverter.getSeconds(stats.getTimePlayed().split(":"));
+        long restTimePlayed = seconds % 7;
+        long totalTimePlayedPerDay = seconds / 7;
+        long restScoredPoints = stats.getTotalScoredPoints() % 7;
+        long totalScoredPointsPerDay = stats.getTotalScoredPoints() / 7;
+        Integer numberOfWonGames = stats.getNumberOfWonGames();
+        return DailyStatsSummed.builder()
+                .day(week.minusDays(i))
+                .mode(mode)
+                .totalGamePlayed(i != 7 ? totalGamePlayedPerDay : totalGamePlayedPerDay + restTotalGamePlayed)
+                .timePlayed(i != 7 ? TimeConverter.convertSecondsToTime(totalTimePlayedPerDay)
+                        : TimeConverter.convertSecondsToTime(totalGamePlayedPerDay + restTimePlayed))
+                .totalScoredPoints(i != 7 ? totalScoredPointsPerDay : totalScoredPointsPerDay + restScoredPoints)
+                .numberOfWonGames(i != 7 ? 0 : numberOfWonGames)
+                .avgScore(stats.getAvgScore().subtract(new BigDecimal(String.format("0.0%d", i))))
+                .build();
+    }
+    @Override
+    public void populateWeek(LocalDate week) {
+        StatsWithUName stats = statsPageService.getStatsOfAllUsersCombinedFromWeek(week);
+        Map<String, StatsWithUName> gameStats = statsPageService.getGameStatsOffAllUsersCombinedFromWeek(week);
+        List<DailyStatsSummed> dailyStatsSummed = new ArrayList<>();
+        for(int i = 1; i <= 7; i++) {
+            dailyStatsSummed.add(getDailyStatsSummedFromDayAndPerMode(stats, i, COMBINED_STATS, week));
+        }
+        for(Mode modeEnum : Mode.values()) {
+            String mode = modeEnum.toString();
+            StatsWithUName gameStat = gameStats.get(mode);
+            for(int i = 1 ; i <= 7; i++) {
+                dailyStatsSummed.add(getDailyStatsSummedFromDayAndPerMode(gameStat, i, mode, week));
+            }
+        }
+        dailyStatsSummedRepository.saveAll(dailyStatsSummed);
+    }
+
+
+    @Override
+    public List<DailyStatsSummed> getDailyStatsSummedForChartPerMode(String mode) {
+        return null;
+    }
+}
