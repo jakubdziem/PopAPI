2c4c5f294343bdd9ce03eaad2bb1cb58a72f5180
Jakub Dziem <jakub.dziem9@gmail.com>
2025-04-10 13:44:17 +0200

    Add: Builder and AllArgsConstructor annotations to GameStatsDTO for testing convenience

--- src/main/java/com/dziem/popapi/model/GameStatsDTO.java
+++ src/main/java/com/dziem/popapi/model/GameStatsDTO.java
@@ -1,8 +1,12 @@
 package com.dziem.popapi.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
+@Builder
 public class GameStatsDTO {
     private Integer timePlayedSeconds;
     private Integer scoredPoints;
